269a59d4b80896193493d9388a03120385ecdfb3
PgmJun <84304802+PgmJun@users.noreply.github.com>
2022-10-02 08:58:45 +0900

    백준 2751번 풀이

---
+++ 백준/2751.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input().rstrip())
+nlist = []
+for _ in range(n):
+    nlist.append(int(input().rstrip()))
+nlist.sort()
+for n in nlist:
+    print(n)
