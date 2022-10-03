aebf7ea0b0d99ed25be7fc285f0d8bab418579ea
PgmJun <84304802+PgmJun@users.noreply.github.com>
2022-10-03 23:45:10 +0900

    백준 7568번 문제 풀이(복습필요)

---
+++ 백준/7568.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input().rstrip())
+dlist = []
+for _ in range(n):
+    dlist.append(list(map(int, input().rstrip().split())))
+
+for d in dlist:
+    rank = 1
+    for i in dlist:
+        if d[0] < i[0] and d[1] < i[1]:
+            rank += 1
+    print(rank, end=' ')
