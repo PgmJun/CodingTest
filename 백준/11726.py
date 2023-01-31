b863d810dd687a9921bee9e52fdbe60837be1ab1
PgmJun <84304802+PgmJun@users.noreply.github.com>
2023-01-31 18:46:03 +0900

    초기값 설정 오류 수정
    dp

--- 백준/11726.py
+++ 백준/11726.py
@@ -1,12 +1,18 @@
+# 점화식
+# dp[i] = dp[i-1]+dp[i-2]
+
+import sys
+input=sys.stdin.readline
+
 n = int(input())
 dp = [0 for _ in range(1001)]
-
+dp[1] = 1
 dp[2] = 2
 dp[3] = 3
 dp[4] = 5
 
-if n > 5:
+if n > 4:
     for i in range(5, n+1):
-        dp[i] = dp[i-1]+dp[i-2]
+        dp[i] = (dp[i-1]+dp[i-2]) % 10007
 
-print(dp[n] % 10007)
+print(dp[n])
