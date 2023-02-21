ca3544e4098e3b1d600551cd0ca6b35f5e632915
JUN <chltmdwns96@naver.com>
2023-02-21 17:00:38 +0900

    백준 10825번 문제 풀이 방식 수정
    Sorting (시간복잡도 고려)

--- 백준/10825.py
+++ 백준/10825.py
@@ -1,8 +1,10 @@
-arr = []
+import sys
+input = sys.stdin.readline
+
+students = []
 for _ in range(int(input())):
-    arr.append(list(input().split()))
+    students.append(list(input().split()))
 
-arr.sort(key=lambda x: (-int(x[1]), int(x[2]), -int(x[3]), x[0]))
 
-for a in arr:
-    print(a[0])
+for s in sorted(students, key=lambda x: (-int(x[1]),int(x[2]),-int(x[3]),x[0])):
+    print(s[0])
