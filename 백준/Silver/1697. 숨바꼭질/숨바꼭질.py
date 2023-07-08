28bdd7113d24c911669f46bbc3c1fa254247b0f3
PgmJun <84304802+PgmJun@users.noreply.github.com>
2023-07-08 22:49:24 +0900

    [Silver I] Title: 숨바꼭질, Time: 140 ms, Memory: 35224 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -16,14 +16,16 @@ def bfs():
 
         if pos == k:
             return delay
-        visited[pos] = True
 
         if checkBoundary(pos+1) and not visited[pos+1]:
             queue.append([pos+1, delay+1])
+            visited[pos+1] = True
         if checkBoundary(pos-1) and not visited[pos-1]:
             queue.append([pos-1, delay+1])
+            visited[pos-1] = True
         if checkBoundary(pos*2) and not visited[pos*2]:
             queue.append([pos*2, delay+1])
+            visited[pos*2] = True
 
     
 n,k = map(int,input().split())
