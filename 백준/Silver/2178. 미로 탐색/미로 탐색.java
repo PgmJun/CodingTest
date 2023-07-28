5d9fc786edfc4869f8941e2ddb1e99450c0f442c
PgmJun <84304802+PgmJun@users.noreply.github.com>
2023-07-29 01:08:58 +0900

    [Silver I] Title: 미로 탐색, Time: 188 ms, Memory: 16664 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -13,45 +12,9 @@ public class Main {
 	public static int[][] graph = new int[MAX][MAX];
 	public static boolean[][] visited = new boolean[MAX][MAX];
 
-	public static int[] dx = {-1,1,0,0};
-	public static int[] dy = {0,0,-1,1};
-
-	public static void bfs(Node start, int n, int m) {
-		queue.add(start);
-
-		while(queue.peek() != null) {
-			Node now = queue.poll();
-			int x = now.getX();
-			int y = now.getY();
-			int depth = now.depth;
-
-			if(x == n && y == m) {
-				System.out.print(depth);
-				break;
-			}
-
-			for(int i = 0;i < 4;i++) {
-				int nx = x + dx[i];
-				int ny = y + dy[i];
-
-				if(nx <= 0 || nx > n || ny <= 0 || ny > m) {
-					continue;
-				}
-				if(visited[nx][ny]) {
-					continue;
-				}
-				if(graph[nx][ny] == 0) {
-					continue;
-				}
-
-				visited[nx][ny] = true;
-				queue.add(new Node(nx,ny,depth+1));
-			}
-
-
-
-		}
-	}
+	public static int[] dx = {-1, 1, 0, 0};
+	public static int[] dy = {0, 0, -1, 1};
+	
 
 	public static void main(String[] args) throws IOException {
 		String[] input = br.readLine().split(" ");
@@ -60,8 +23,42 @@ public class Main {
 
 		initGraph(n);
 		initVisited();
-		bfs(new Node(1,1,1), n, m);
+		bfs(new Node(1, 1, 1), n, m);
 
+	}
+
+	public static void bfs(Node start, int n, int m) {
+		queue.add(start);
+
+		while (queue.peek() != null) {
+			Node now = queue.poll();
+			int x = now.getX();
+			int y = now.getY();
+			int depth = now.depth;
+
+			if (x == n && y == m) {
+				System.out.print(depth);
+				break;
+			}
+
+			for (int i = 0; i < 4; i++) {
+				int nx = x + dx[i];
+				int ny = y + dy[i];
+
+				if (nx <= 0 || nx > n || ny <= 0 || ny > m) {
+					continue;
+				}
+				if (visited[nx][ny]) {
+					continue;
+				}
+				if (graph[nx][ny] == 0) {
+					continue;
+				}
+
+				visited[nx][ny] = true;
+				queue.add(new Node(nx, ny, depth + 1));
+			}
+		}
 	}
 
 	private static void initGraph(int n) throws IOException {
