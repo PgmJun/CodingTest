4cdc1703e88a53a08747bbc4596fe94c1cd7979f
PgmJun <84304802+PgmJun@users.noreply.github.com>
2023-08-04 15:52:17 +0900

    문제 해결 아이디어 추가

--- 프로그래머스/lv3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/lv3/92343. 양과 늑대/양과 늑대.java
@@ -1,3 +1,14 @@
+// [문제 해결 아이디어]
+// 모든 경우를 탐색하기 때문에 dfs 활용
+
+// 0번 인덱스 부터 시작하므로 dfs의 idx값에 0을 넣어 시작
+// 방문한 인덱스를 visited 체크
+// 방문한 인덱스의 info가 0(양)이면 sheepCount++, maxSheepCount체크
+// 방문한 인덱스의 info가 1(늑대)이면 wolfCount++
+//
+// 부모는 방문했지만 자식노드는 방문하지 않은 모든 노드를 찾아 방문한다.
+// 모든 경우를 탐색하기 위해 현재 visited 를 완전카피하여 dfs
+
 class Solution {
     
 	static int[] gInfo;
