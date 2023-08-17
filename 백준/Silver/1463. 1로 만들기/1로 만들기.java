eb228de3ab96717b242259187bef10e564c78d43
PgmJun <84304802+PgmJun@users.noreply.github.com>
2023-08-17 12:54:16 +0900

    [Silver III] Title: 1로 만들기, Time: 140 ms, Memory: 18172 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -4,35 +4,24 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-
-	public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 	public static void main(String[] args) throws IOException {
-		int x = input();
-		int[] dp = new int[x+1];
-		initDp(dp, x);
+		int n = Integer.parseInt(br.readLine());
+		int[] dp = new int[1000001];
 
-		for (int i = 2; i < x+1; i++) {
+		dp[2] = 1;
+		dp[3] = 1;
+
+		for (int i = 4; i <= n; i++) {
 			dp[i] = dp[i-1]+1;
-			if (i % 2 == 0) {
-				dp[i] = Math.min(dp[i], dp[i/2]+1);
-			}
-			if (i % 3 == 0) {
-				dp[i] = Math.min(dp[i], dp[i/3]+1);
+			if(i % 2 == 0) {
+				dp[i] = Math.min(dp[i / 2] + 1, dp[i]);
+			} if(i % 3 == 0) {
+				dp[i] = Math.min(dp[i / 3] + 1, dp[i]);
 			}
 		}
 
-		System.out.print(dp[x]);
-
-	}
-
-	private static int input() throws IOException {
-		return Integer.parseInt(br.readLine());
-	}
-
-	private static void initDp(int[] dp, int x) {
-		for (int i = 0; i < x+1; i++) {
-			dp[i] = 0;
-		}
+		System.out.println(dp[n]);
 	}
 }
