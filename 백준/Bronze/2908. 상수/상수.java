9bbb5bab4b19f9b36059a3fb2b0f2ffab3a7a5ba
Eden <84304802+PgmJun@users.noreply.github.com>
2024-07-04 18:15:02 +0900

    [Bronze II] Title: 상수, Time: 68 ms, Memory: 11480 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.java
+++ 백준/Bronze/2908. 상수/상수.java
@@ -4,16 +4,16 @@ class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] ab = br.readLine().split(" ");
-        String[] a = ab[0].split("");
-        String[] b = ab[1].split("");
+        String a = ab[0];
+        String b = ab[1];
 
-        int reserveA = Integer.parseInt(a[2] + a[1] + a[0]);
-        int reserveB = Integer.parseInt(b[2] + b[1] + b[0]);
+        int aValue = Integer.parseInt(new StringBuilder(a).reverse().toString());
+        int bValue = Integer.parseInt(new StringBuilder(b).reverse().toString());
 
-        if(reserveA > reserveB) {
-            System.out.println(reserveA);
+        if(aValue > bValue) {
+            System.out.println(aValue);
         } else {
-            System.out.println(reserveB);
+            System.out.println(bValue);
         }
     }
 }
