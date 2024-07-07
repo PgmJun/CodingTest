bc1b1b6d48c16c9fd4591e63e17224f226218849
Eden <84304802+PgmJun@users.noreply.github.com>
2024-07-07 20:44:30 +0900

    [Bronze I] Title: 단어 공부, Time: 132 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,42 +1,29 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
 
 class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String input = br.readLine().toUpperCase();
-        String[] words = input.split("");
-
-        Map<String, Integer> wordMap = new HashMap<>();
-        for (String word : words) {
-            wordMap.put(word, wordMap.getOrDefault(word, 0) + 1);
-        }
-
-        LinkedList<Map.Entry<String, Integer>> entries = new LinkedList<>(wordMap.entrySet());
-        entries.sort(new Comparator<Map.Entry<String, Integer>>() {
-            @Override
-            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
-                return o2.getValue() - o1.getValue();
-            }
-        });
-
-        String answer = "";
-        int maxSize = entries.getFirst().getValue();
-        if (entries.size() == 1) {
-            answer = entries.getFirst().getKey();
-        } else if (entries.size() > 1) {
-            if (entries.get(1).getValue() == maxSize) {
-                answer = "?";
+        int c = System.in.read();
+        int[] arr = new int[26];
+        while(c > 64) {
+            if(c < 91) {
+                arr[c-65]++;
             } else {
-                answer = entries.getFirst().getKey();
+                arr[c-97]++;
+            }
+            c = System.in.read();
+        }
+
+        int max = -1;
+        int answer = -2;
+        for(int i = 0; i < 26; i++) {
+            if(arr[i] > max) {
+                answer = i;
+                max = arr[i];
+            } else if(arr[i] == max) {
+                answer = -2;
             }
         }
 
-        System.out.println(answer);
+        System.out.println((char)(answer + 65));
     }
 }
