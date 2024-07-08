df2793379e45b77c14c32f78570b84bee07266e2
Eden <84304802+PgmJun@users.noreply.github.com>
2024-07-08 23:37:34 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 104 ms, Memory: 14380 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -1,8 +1,4 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
+import java.io.*;
 
 class Main {
     public static void main(String[] args) throws IOException {
@@ -11,26 +7,20 @@ class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < n; i++) {
-            boolean isGroupWord = true;
-            String[] words = br.readLine().split("");
-            String before = "";
-            Set<String> beforeWords = new HashSet<>();
+        L:for (int i = 0; i < n; i++) {
+            boolean[] isGroupWord = new boolean[27];
+            String word = br.readLine();
 
-            for (int j = 1; j < words.length; j++) {
-                before = words[j - 1];
-                beforeWords.add(words[j - 1]);
-
-                String now = words[j];
-                if (beforeWords.contains(now) && !before.equals(now)) {
-                    isGroupWord = false;
-                    break;
+            for (int j = 1; j < word.length(); j++) {
+                if(isGroupWord[word.charAt(j) - 'a']) {
+                    continue L;
+                }
+                if(word.charAt(j) != word.charAt(j-1)) {
+                    isGroupWord[word.charAt(j-1) - 'a'] = true;
                 }
             }
 
-            if(isGroupWord) {
-                answer++;
-            }
+            answer++;
         }
 
         System.out.println(answer);
