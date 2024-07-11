feb0494d20b9d121357d5bab5a648028c7e90da3
Eden <84304802+PgmJun@users.noreply.github.com>
2024-07-11 20:34:34 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 72 ms, Memory: 11724 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -6,31 +6,32 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
         int n = Integer.parseInt(br.readLine());
-
         for (int i = 1; i <= n; i++) {
             int mid = 2 * i - 1;
             int left = n - i;
             for (int j = 0; j < left; j++) {
-                System.out.print(" ");
+                sb.append(" ");
             }
             for (int j = 0; j < mid; j++) {
-                System.out.print("*");
+                sb.append("*");
             }
-            System.out.println();
+            sb.append("\n");
         }
 
         for (int i = n - 1; i > 0; i--) {
             int mid = 2 * i - 1;
             int left = n - i;
             for (int j = 0; j < left; j++) {
-                System.out.print(" ");
+                sb.append(" ");
             }
             for (int j = 0; j < mid; j++) {
-                System.out.print("*");
+                sb.append("*");
             }
-            System.out.println();
+            sb.append("\n");
         }
+        System.out.println(sb);
     }
 }
